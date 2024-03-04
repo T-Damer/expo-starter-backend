b5cbf7eb0261ff0f585dc72731dcd36c6ce524ff
Daniil Pankov <dpancov64@gmail.com>
2024-03-04 18:08:47 +0300

    Run Fix prisma client integration

--- src/helpers/runApp.ts
+++ src/helpers/runApp.ts
@@ -4,6 +4,7 @@ import { createYoga } from 'graphql-yoga'
 import { resolvers } from '@generated/type-graphql'
 import { buildSchema } from 'type-graphql'
 import path from 'path'
+import prismaClient from './prismaClient'
 
 export default async function () {
   const schema = buildSchema({
@@ -13,18 +14,12 @@ export default async function () {
   })
 
   const yoga = createYoga({
+    context: { prisma: prismaClient },
     schema,
   })
 
   const server = createServer(yoga.requestListener)
 
-  return new Promise<Server>((resolve, reject) => {
-    const connection = server
-      .listen(env.SERVER_PORT)
-      .on('listening', () => {
-        console.log(`HTTP is listening on https://localhost:${env.SERVER_PORT}`)
-        resolve(connection)
-      })
-      .on('error', reject)
-  })
+  server.listen(env.SERVER_PORT)
+  console.log('Listening on http://localhost:', env.SERVER_PORT)
 }
--- src/resolvers/posts.ts
+++ src/resolvers/posts.ts
@@ -6,14 +6,12 @@ import {
   PostCreateInput,
 } from '@generated/type-graphql'
 import prismaClient from '@/helpers/prismaClient'
-import { Prisma } from '@prisma/client'
 import all from '@generated/type-graphql/resolvers/crud/Post/PostCrudResolver'
 
 @Resolver()
 export default class PostResolver {
   @Mutation(() => Post)
-  addPost() // postData: Without<PostUncheckedCreateInput, PostCreateInput> // @Arg('postData')
-  {
+  addPost() { // postData: Without<PostUncheckedCreateInput, PostCreateInput> // @Arg('postData')
     // console.info('Adding post', { postData })
     // return prismaClient.post.create({ data: postData })
   }
